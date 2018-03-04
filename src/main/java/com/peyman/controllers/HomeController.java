fb2977529550a96f309f274dda65aacb5afeaf60
Peymandodangeh@gmail.com <asemanEABI@@90>
2018-03-04 15:43:14 +0330

    add project page and controller

--- src/main/java/com/peyman/controllers/HomeController.java
+++ src/main/java/com/peyman/controllers/HomeController.java
@@ -7,13 +7,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Peyman on 3/4/2018.
- */@Controller
+ */
+@Controller
+
 public class HomeController {
     private static final Logger logger = Logger.getLogger(HomeController.class);
     @RequestMapping("/home")
     @ResponseBody
     public String goHome(){
-        logger.info("salam");
+        logger.info("in go home method");
         return "Welcome Home!!!";
     }
+
+    @RequestMapping("/home2")
+    public String goHome2(){
+        logger.info("salam");
+        return "home";
+    }
+
 }
